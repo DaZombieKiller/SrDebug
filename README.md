b088b1e7b0edb05ca7ddff8d2f0d92fc76c41c4e
DaZombieKiller <bennyboy.private@hotmail.com.au>
2017-06-16 01:39:17 +1000

    Attempt at MacOS X support (untested)

--- README.md
+++ README.md
@@ -10,16 +10,18 @@ A debug menu for Slime Rancher
 3. Wait for the patch tool to complete, then run _Slime Rancher_
 
 ### Custom built
-1. Browse to **Source/Assembly-SrDebug/bin/.../\<Build Type\>/** where \<Build Type\> can be *Debug* or *Release*, depending on how you compiled.
-2. Copy _Assembly-SrDebug.dll_ to the **SlimeRancher_Data/Managed/** folder in your _Slime Rancher_ game directory
-3. Browse to **Source/SrDebug-Patcher/bin/.../\<Build Type\>/** where \<Build Type\> can be *Debug* or *Release*, depending on how you compiled.
-4. Copy _Mono.Cecil.dll_ and _SrDebug-Patcher.exe_ to your _Slime Rancher_ game directory
-5. Run `SrDebug-Patcher.exe`, or `mono SrDebug-Patcher.exe` for non-Windows
-6. Wait for the patch tool to complete, then run _Slime Rancher_
+1. Create a folder named **SrDebug-Content** in your _Slime Rancher_ game folder
+2. Browse to **Source/Assembly-SrDebug/bin/.../\<Build Type\>/** where \<Build Type\> can be *Debug* or *Release*, depending on how you compiled
+3. Copy _Assembly-SrDebug.dll_ to the **SrDebug-Content** folder in your _Slime Rancher_ game directory
+4. Browse to **Source/SrDebug-Patcher/bin/.../\<Build Type\>/** where \<Build Type\> can be *Debug* or *Release*, depending on how you compiled.
+5. Copy _Mono.Cecil.dll_ to the **SrDebug-Content** directory
+6. Copy _SrDebug-Patcher.exe_ and _SrDebug-Patcher.exe.config_ to your _Slime Rancher_ game directory
+6. Run `SrDebug-Patcher.exe`, or `mono SrDebug-Patcher.exe` for non-Windows
+7. Wait for the patch tool to complete, then run _Slime Rancher_
 
 # Building
 1. Browse to your _Slime Rancher_ game directory (usually **SteamApps/common/Slime Rancher/**)
-2. Enter the **SlimeRancher_Data/Managed/** folder
+2. Locate the game's _Managed_ folder (SlimeRancher_Data/Managed on Windows & Linux, Content/Resources/Data/Managed on Mac)
 3. Copy _UnityEngine.dll_ and _Assembly-CSharp.dll_ into the _Managed_ folder in the _SrDebug_ repository root
 4. Run `generate.bat` or `Protobuild --generate` for non-Windows
 5. Build the resultant solution file
--- Source/SrDebug-Patcher/Program.cs
+++ Source/SrDebug-Patcher/Program.cs
@@ -7,18 +7,42 @@ using Mono.Cecil.Cil;
 
 internal class Program
 {
-    private const string DataPath = "SlimeRancher_Data";
+    private const string ContentPath = "SrDebug-Content";
+    private static string _dataPath;
     private static Dictionary<string, ModuleDefinition> _modules;
     private static Dictionary<string, string> _modulePaths;
     private static DefaultAssemblyResolver _resolver;
+
+    private static readonly string[] PotentialDataPaths =
+    {
+        "Content/Resources/Data",
+        "Resources/Data",
+        "Data",
+        "SlimeRancher_Data",
+    };
     
     /// <summary>The program entry point.</summary>
     private static void Main()
     {
+        // search for the data path
+        foreach (var path in PotentialDataPaths)
+        {
+            if (!Directory.Exists(path)) continue;
+            _dataPath = path;
+            break;
+        }
+        
+        // did we find it?
+        if (!Directory.Exists(_dataPath))
+            throw new DirectoryNotFoundException("Could not locate the Data folder.");
+
         _resolver = new DefaultAssemblyResolver();
-        _resolver.AddSearchDirectory($"{DataPath}/Managed");
+        _resolver.AddSearchDirectory($"{_dataPath}/Managed");
         _modulePaths = new Dictionary<string, string>();
         _modules = new Dictionary<string, ModuleDefinition>();
+        
+        // copy SrDebug assembly to managed folder
+        File.Copy($"{ContentPath}/Assembly-SrDebug.dll", $"{_dataPath}/Managed/Assembly-SrDebug.dll", true);
 
         // load game assembly
         var targetModule = LoadModule("Assembly-CSharp");
@@ -57,7 +81,7 @@ internal class Program
     private static ModuleDefinition LoadModule(string name)
     {
         if (_modules.ContainsKey(name)) return _modules[name];
-        _modulePaths[name] = $"{DataPath}/Managed/{name}.dll";
+        _modulePaths[name] = $"{_dataPath}/Managed/{name}.dll";
         _modules[name] = ModuleDefinition.ReadModule(
             new MemoryStream(File.ReadAllBytes(_modulePaths[name])),
             new ReaderParameters { AssemblyResolver = _resolver });
